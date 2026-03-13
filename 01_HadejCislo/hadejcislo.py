5bbce70b572b5d464096ff3b4cc0f82316cc733b
Vase Jmeno <jakub.drobisz@tria-tr.cz>
2026-03-13 09:51:59 +0100

    Přidán kód hry Hádej číslo a dokumentace projektu

--- 01_HadejCislo/hadejcislo.py
+++ 01_HadejCislo/hadejcislo.py
@@ -1,21 +1,40 @@
 #budu delat kod pro hadani cisla
 import random
+
 def hra():
     print("Vítej ve hře hádej číslo!")
     print("Myslím si číslo mezi 1 a 100. Zkus ho uhodnout!")
     cislo = random.randint(1, 100)
     pokusy = 8
     while pokusy > 0:
-        hadani = int(input("Zadej svůj tip: "))
+        try:
+            hadani = int(input("Zadej svůj tip: "))
+        except ValueError:
+            print("To není platné číslo! Zkus to znovu.")
+            continue
         if hadani < cislo:
             print("Moc nízké! Zkus to znovu.")
         elif hadani > cislo:
             print("Moc vysoké! Zkus to znovu.")
         else:
-            print(f"Gratulace! Uhodl jsi číslo {cislo}!")
-            break
+            print(f"Gratulace! Uhodl jsi číslo {cislo} za {9 - pokusy} pokusů!")
+            return True
         pokusy -= 1
+        print(f"Zbývá ti {pokusy} pokusů.")
     else:
         print(f"Prohrál jsi! Myslel jsem si číslo {cislo}.")
+        return False
 
-hra()
+def main():
+    while True:
+        vyhra = hra()
+        if vyhra:
+            print("Skvělé! Chceš hrát znovu? (ano/ne)")
+        else:
+            print("Nevadí, zkus to znovu! Chceš hrát znovu? (ano/ne)")
+        odpoved = input().lower()
+        if odpoved != "ano":
+            print("Díky za hru!")
+            break
+
+main()
