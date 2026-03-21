ca8ad02f30e02aa6f5c9c6b3528b02e73dbdc74a
drobek <jakub.drobisz@tria-tr.cz>
2026-03-21 21:24:42 +0100

    Refactor task management in todolist.py

--- 02_todolist/todolist.py
+++ 02_todolist/todolist.py
@@ -1,11 +1,13 @@
-#seznam pro ukládání úkolů 
+# seznam pro ukládání úkolů (úkol + stav splnění)
 def zobraz_ukoly(ukoly):
     if not ukoly:
-        print(" Žádné úkoly")
+        print("Žádné úkoly")
     else:
-        print("\n Seznam úkolů:")
+        print("\nSeznam úkolů:")
         for i, ukol in enumerate(ukoly, 1):
-            print(f"{i}. {ukol}")
+            stav = "✔ splněno" if ukol["hotovo"] else "❌ nesplněno"
+            print(f"{i}. {ukol['text']} [{stav}]")
+
 
 def main():
     ukoly = []
@@ -15,14 +17,15 @@ def main():
         print("1 - Přidat úkol")
         print("2 - Zobrazit úkoly")
         print("3 - Smazat úkol")
-        print("4 - Konec")
+        print("4 - Označit jako splněné")
+        print("5 - Konec")
 
         volba = input("Vyber: ")
 
         if volba == "1":
-            ukol = input("Zadej úkol: ")
-            ukoly.append(ukol)
-            print(" Úkol přidán")
+            text = input("Zadej úkol: ")
+            ukoly.append({"text": text, "hotovo": False})
+            print("Úkol přidán")
 
         elif volba == "2":
             zobraz_ukoly(ukoly)
@@ -32,13 +35,25 @@ def main():
             try:
                 cislo = int(input("Který úkol chceš smazat? "))
                 ukoly.pop(cislo - 1)
-                print(" Úkol smazán")
+                print("Úkol smazán")
             except:
-                print(" Špatné číslo")
+                print("Špatné číslo")
 
         elif volba == "4":
-            print("👋 Konec programu")
+            zobraz_ukoly(ukoly)
+            try:
+                cislo = int(input("Který úkol je splněný? "))
+                ukoly[cislo - 1]["hotovo"] = True
+                print("Úkol označen jako splněný")
+            except:
+                print("Špatné číslo")
+
+        elif volba == "5":
+            print("Konec programu 👋")
             break
 
         else:
-            print(" Neplatná
+            print("Neplatná volba")
+
+
+main()
